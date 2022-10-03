6bb5509da3b8b6f46a572c8c0b824f429c14ceb5
emmanuelbogacz <114598623+emmanuelbogacz@users.noreply.github.com>
2022-10-03 16:37:39 +0100

    practical 3 files upload

---
+++ practical03/integral.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int a,N,i; //initialisation
+    a=0;
+    N=13;
+
+    float sum,b;
+    b=M_PI/3;
+    sum=0;
+
+    sum=tan(a)+tan(b); //endpoints
+
+    for(i=0;i<N;i++){
+    sum=sum + 2*tan(a+ i*(b-a)/N); //the argument of tan is each equidistant point, i is the parameter
+    }
+
+    sum=(b-a)*sum/(2*N); //prefactors
+
+    printf("Calucated integral = %f \n", sum);
+    printf("Exact solution = %f \n", log(2));
+    printf("Difference = %f \n", fabs(sum-log(2)) ); //fabs is the math.h absolute value
+    
+    
+return(0);
+}
