4509ac4e45df4ea2d4be0907df6e39897549fd51
emmanuelbogacz <114598623+emmanuelbogacz@users.noreply.github.com>
2022-10-18 11:36:38 +0100

    corrected bug (values for the series)

--- practical05/fibonacci.f90
+++ practical05/fibonacci.f90
@@ -1,31 +1,5 @@
-!
-! Commented out is the old code (more basic)
-!
-
-! program fibonacci
-! integer(4):: n,i,temp,a,b
-!     
-! write(6,*) "Please input an integer: "
-! 
-! read(5,"(i3)") n
-!     
-! a=0
-! b=1
-!     
-! write(6,"(i11)") a
-!     
-! do i=3,n,1  
-! temp=a
-! a=a+b !this is the updated F_n
-! b=temp !this is the updated F_n-1
-!     
-! write(6,"(i11)") a
-! end do
-! 
-! end program fibonacci
-
 module global
-    integer(4)::a,b,temp
+    integer(16)::a,b,temp
     
 end module global
 
@@ -33,19 +7,15 @@ program fibonacci
 use global
 
     integer(4):: n,i
-        
     a=0
     b=1
         
     write(6,*) "Please input an integer: "
+    read(5,*) n
 
-    read(5,"(i3)") n
-
-    write(6,"(i11)") a
-        
     do i=3,n,1  
         call update_a_b
-        write(6,"(i11)") a
+        write(6,*) b
     end do
 
 end program fibonacci
